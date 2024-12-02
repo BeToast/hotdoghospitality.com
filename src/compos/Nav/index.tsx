e3dfc540c517ca812f8e0a91757d1eae1340bcba
Blake Preston <note2bp@protonmail.com>
2024-12-02 00:11:01 -0700

    fade out nav

--- src/compos/Nav/index.tsx
+++ src/compos/Nav/index.tsx
@@ -16,6 +16,17 @@ const Nav: React.FC = () => {
       return () => unsubscribe();
    }, [router]);
 
+   const [visible, setVisible] = useState(true);
+
+   useEffect(() => {
+      const handleScroll = () => {
+         setVisible(window.scrollY < 100);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+   }, []);
+
    const navItems: NavItemInter[] = [
       { path: "/home", label: "Home" },
       { path: "/about", label: "About" },
@@ -26,7 +37,12 @@ const Nav: React.FC = () => {
    ];
 
    return (
-      <nav className="fixed top-0 left-0 w-full px-4 py-3 h-[102px]z-20">
+      // <nav className="fixed top-0 left-0 w-full px-4 py-3 h-[102px]z-20">
+      <nav
+         className={`fixed top-0 left-0 w-full px-4 py-3 h-[102px] z-20 transition-opacity duration-300 ${
+            visible ? "opacity-100" : "opacity-0"
+         }`}
+      >
          <div className="flex justify-center gap-8">
             {navItems.map((item) => (
                <NavItem
